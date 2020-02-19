1ff3cafb2b42d135d9c229cbf2a8918a8dcf65d8
devmullets <devmullets@gmail.com>
2020-02-19 14:22:42 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+    #code to describe 'dog' class
+end
+
+fido = Dog.new
+lassie = Dog.new
+snoopy = Dog.new
+
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+    #code for 'person' class goes here
+
+end 
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+
