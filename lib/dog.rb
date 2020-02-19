665bf7cf7be7b6f296e492b24088bb3688c081d3
lucassherwin <lksherwin@gmail.com>
2020-02-19 14:27:47 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+#define dog class
+class Dog
+end
+
+#make 3 dogs; fido, snoopy, lassie
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+#define person class
+class Person
+end
+
+#define 2 people; adele_goldberg and alan_kay
+adele_goldberg = Person.new
+alan_kay = Person.new
